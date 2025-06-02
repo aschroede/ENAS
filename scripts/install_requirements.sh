27bfe4f0f283c24cf8f4854692578081efd7bfd8
aschroede <aschroed@ualberta.ca>
2025-06-02 17:22:27 +0200

    Added install requirements.sh

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -1,6 +1,6 @@
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd "$SCRIPT_DIR" || exit 1
+# cd "$SCRIPT_DIR" || exit 1
 
-echo "$SCRIPT_DIR"
+# echo "$SCRIPT_DIR"
 pip install -r requirements.txt
