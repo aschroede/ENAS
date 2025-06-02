6a46e2152f7251a6e00ed59135ca9df5672946f8
aschroede <aschroed@ualberta.ca>
2025-06-02 17:29:36 +0200

    Added download data script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,15 +1,9 @@
+
 #!/bin/bash
 
-# Set destination path (edit if needed)
-DEST_DIR="./data"
-FILENAME="NAS-Bench-201-v1_1-096897.pth"
-URL="https://github.com/D-X-Y/AutoDL-Projects/releases/download/NAS-Bench-201-v1_1/$FILENAME"
+# Ensure gdown is installed
+pip install --quiet gdown
 
-# Create the directory if it doesn't exist
-mkdir -p "$DEST_DIR"
+# Download NAS-Bench-201-v1_1-096897.pth
+gdown --id 16Y0UwGisiouVRxW-W5hEtbxmcHw_0hF_ --output .data/NAS-Bench-201-v1_1-096897.pth
 
-# Download the file
-echo "Downloading NAS-Bench-201 to $DEST_DIR/$FILENAME..."
-wget -O "$DEST_DIR/$FILENAME" "$URL"
-
-echo "Download complete."
