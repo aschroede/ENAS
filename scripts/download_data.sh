d1221daa54b9125d3c17dcde741ad28157a40fd5
aschroede <aschroed@ualberta.ca>
2025-06-02 17:31:57 +0200

    Added download data script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -5,5 +5,5 @@
 pip install --quiet gdown
 
 # Download NAS-Bench-201-v1_1-096897.pth
-gdown --id 16Y0UwGisiouVRxW-W5hEtbxmcHw_0hF_ --output .data/NAS-Bench-201-v1_1-096897.pth
+gdown --id 16Y0UwGisiouVRxW-W5hEtbxmcHw_0hF_ --output ./data/NAS-Bench-201-v1_1-096897.pth
 
